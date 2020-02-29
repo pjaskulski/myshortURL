d116acc5877c63ac058f3bcba2bc7f49ca2ae090
Piotr Jaskulski <piotr.jaskulski@gmail.com>
2020-02-29 22:18:31 +0100

    new tables, migrations

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,4 @@
-from myshorturl import db, Link
+from myshorturl import db
 import os
 from urllib.parse import unquote
 
@@ -6,8 +6,4 @@ from urllib.parse import unquote
 basedir = os.path.abspath(os.path.dirname(__file__))
 if not os.path.exists(os.path.join(basedir, 'links.db')):
     db.create_all()
-    idlink = 'qwertyuiop'
-    adres = 'https%3A%2F%2Fflask-sqlalchemy.palletsprojects.com%2Fen%2F2.x%2Fquickstart%2F%23a-minimal-application'
-    my_link = Link(idlink, unquote(adres))
-    db.session.add(my_link)
     db.session.commit()
