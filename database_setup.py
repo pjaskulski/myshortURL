e23b840e7e73dac1c58d5055cbee2aeea3d68ac7
Piotr Jaskulski <piotr.jaskulski@gmail.com>
2020-02-29 22:31:57 +0100

    database change

--- database_setup.py
+++ database_setup.py
@@ -4,6 +4,6 @@ from urllib.parse import unquote
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-if not os.path.exists(os.path.join(basedir, 'links.db')):
+if not os.path.exists(os.path.join(basedir, 'links.sqlite')):
     db.create_all()
     db.session.commit()
--- myshorturl.py
+++ myshorturl.py
@@ -19,7 +19,7 @@ app = Flask(__name__,
             static_folder="static")
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(basedir, 'links.db')
+    os.path.join(basedir, 'links.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'this jacket is blue'
 db = SQLAlchemy(app)
